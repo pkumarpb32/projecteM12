252ef61d78ccc637caef3bfe0e19d94a1829a345
pkumarpb32 <91206941+pkumarpb32@users.noreply.github.com>
2021-11-03 17:02:05 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+let nom = document.getElementById("nom")
+let cognom = document.getElementById("cognom")
+let boto = document.getElementById("guardar")
+let titol = document.getElementById("titol")
+
+let nomAnterior = JSON.parse( window.localStorage.getItem('nom'))
+if(nomAnterior != undefined){
+    titol.innerHTML= "Hola " + nomAnterior.nom + " " + nomAnterior.cognom
+}
+
+boto.addEventListener("click", ()=>{
+    titol.innerHTML= "Hola " + nom.value + " " + cognom.value
+    let dada = {nom: nom.value, cognom: cognom.value}
+    window.localStorage.setItem('nom', JSON.stringify(dada))
+
+
+})
