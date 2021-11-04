b66ed04613670178e9c6f2ce67460c1af728b1f3
Adrian240 <adrian.blaugrana@gmail.com>
2021-11-04 17:06:27 +0100

    a
    ada

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
-let nom = document.getElementById("nom")
-let cognom = document.getElementById("cognom")
+
 let boto = document.getElementById("guardar")
-let titol = document.getElementById("titol")
 
-let nomAnterior = JSON.parse( window.localStorage.getItem('nom'))
 
-if(nomAnterior != undefined){
-    titol.innerHTML= "Hola " + nomAnterior.nom + " " + nomAnterior.cognom
-}
+
+
+
+
+
+
+
+// let nomAnterior = JSON.parse( window.localStorage.getItem('nom'))
+
+// if(nomAnterior != undefined){
+//     titol.innerHTML= "Hola " + nomAnterior.nom + " " + nomAnterior.cognom
+// }
+
+
 
 boto.addEventListener("click", ()=>{
     titol.innerHTML= "Hola " + nom.value + " " + cognom.value
