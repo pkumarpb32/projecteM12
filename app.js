064bf4ac8a253b29338a9dc66363877b61ce67b1
Adrian240 <adrian.blaugrana@gmail.com>
2021-11-05 16:02:20 +0100

    q
    q

--- app.js
+++ app.js
@@ -40,3 +40,44 @@ function drop(ev) {
     var data = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(data));
 }
+
+
+function createTask(){
+    var x = document.getElementById("doing");
+    var y = document.getElementById("done");
+    var z = document.getElementById("create-new-task-block");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+        y.style.display = "block";
+        z.style.display = "none";
+    } else {
+        x.style.display = "none";
+        y.style.display = "none";
+        z.style.display = "flex";
+    }
+}
+
+function saveTask(){
+    
+
+    var todo = document.getElementById("todo");
+    var taskName = document.getElementById("task-name").value;
+    todo.innerHTML += `
+    <div class="task" id="${taskName.toLowerCase().split(" ").join("")}" draggable="true" ondragstart="drag(event)">
+        <span>${taskName}</span>
+    </div>
+    `
+}
+
+
+function editTask(){
+    var saveButton = document.getElementById("save-button");
+    var editButton = document.getElementById("edit-button");
+    if (saveButton.style.display === "none") {
+        saveButton.style.display = "block";
+        editButton.style.display = "none";
+    } else{
+        saveButton.style.display = "none";
+        editButton.style.display = "block";
+    }
+}
