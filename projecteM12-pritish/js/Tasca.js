f19c29d36e33de7b15b37abdbc737040b7676f21
Adrian240 <38760855+Adrian240@users.noreply.github.com>
2021-11-24 17:11:17 +0100

    informacio de tasques

---
+++ projecteM12-pritish/js/Tasca.js
@@ -0,0 +1,70 @@
+export default class Tasca{
+    constructor(){
+    this.codi = ""
+    this.nom = ""
+    this.data_creacio = ""
+    this.data_previsio = ""
+    this.id_responsable = ""
+    this.descripcio = " "
+    this.estat = " "
+    this.prioritat = ""
+    }
+
+// // get-set codi
+//     set Codi(val){
+//         this.codi = val;
+//       }
+//       get Codi(){
+//          return this.codi;
+//       }
+// // get-set nom
+//       set Nom(val){
+//         this.nom = val;
+//       }
+//       get Nom(){
+//          return this.nom;
+//       }
+
+// // get-set descripcio
+//       set Descripcio(val){
+//         this.descripcio = val;
+//       }
+//       get Descripcio(){
+//          return this.descripcio;
+//       }
+// // get-set data creació
+//       set Data_creacio(val){
+//         this.data_creacio = val;
+//       }
+//       get Data_creacio(){
+//          return this.data_creacio;
+//       }
+
+// // get-set data de previsió de finalització
+//       set Data_previsio(val){
+//         this.data_previsio = val;
+//       }
+//       get Data_previsio(){
+//          return this.data_previsio;
+//       }
+
+// // get-set nom responsable
+
+// set Id_responsable(val){
+//     this.id_responsable = val;
+//   }
+//   get Id_responsable(){
+//      return this.id_responsable;
+//   }
+
+//   set Estat(val){
+//    this.estat = val;
+//  }
+//  get Estat(){
+//     return this.estat;
+//  }
+
+
+
+
+}
